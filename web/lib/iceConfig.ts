975cad459eaec096215b6576444fbfbd9f494fb6
Hussein Hakak <quantum.leaps.io.25@gmail.com>
2026-04-19 01:10:52 +1000

    fix: uncomment turn server block

--- web/lib/iceConfig.ts
+++ web/lib/iceConfig.ts
@@ -12,11 +12,9 @@ export const ICE_SERVERS: RTCIceServer[] = [
   { urls: "stun:stun.l.google.com:19302" },
   { urls: "stun:stun1.l.google.com:19302" },
   { urls: "stun:stun2.l.google.com:19302" },
-  /* 
   {
     urls: "turn:your-domain.metered.ca:80",
     username: "e32ec10f28ea739049978679",
     credential: "Tw/jilyBiUtJqr5F",
   },
-  */
 ];
